185a0f7e8439b66909d1ccc4016add0288e9c7c0
dulhara <wkldulhara@gmail.com>
2025-03-29 23:51:15 +0530

    updated

--- lorenz.py
+++ lorenz.py
@@ -1,26 +1,26 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-# def lorenz(xyz, *, s=10, r=28, b=2.667):
-#     """
-#     Parameters
-#     ----------
-#     xyz : array-like, shape (3,)
-#        Point of interest in three-dimensional space.
-#     s, r, b : float
-#        Parameters defining the Lorenz attractor.
+def lorenz(xyz, *, s=10, r=28, b=2.667):
+    """
+    Parameters
+    ----------
+    xyz : array-like, shape (3,)
+       Point of interest in three-dimensional space.
+    s, r, b : float
+       Parameters defining the Lorenz attractor.
 
-#     Returns
-#     -------
-#     xyz_dot : array, shape (3,)
-#        Values of the Lorenz attractor's partial derivatives at *xyz*.
-#     """
-#     x, y, z = xyz
-#     x_dot = s*(y - x)
-#     y_dot = r*x - y - x*z
-#     z_dot = x*y - b*z
-#     return np.array([x_dot, y_dot, z_dot])
+    Returns
+    -------
+    xyz_dot : array, shape (3,)
+       Values of the Lorenz attractor's partial derivatives at *xyz*.
+    """
+    x, y, z = xyz
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
+    return np.array([x_dot, y_dot, z_dot])
 
 
 dt = 0.01

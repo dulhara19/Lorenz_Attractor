b82a51f364b4c5fd689200b859868cbcff51b325
dulhara <wkldulhara@gmail.com>
2025-03-29 23:50:12 +0530

    up

--- lorenz.py
+++ lorenz.py
@@ -36,13 +36,13 @@
 # # Plot
 # ax = plt.figure().add_subplot(projection='3d')
 
-# ax.plot(*xyzs.T, lw=0.5)
-# ax.set_xlabel("X Axis")
-# ax.set_ylabel("Y Axis")
-# ax.set_zlabel("Z Axis")
-# ax.set_title("Lorenz Attractor")
+ax.plot(*xyzs.T, lw=0.5)
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+ax.set_zlabel("Z Axis")
+ax.set_title("Lorenz Attractor")
 
-# plt.show()
+plt.show()
 
 # ###############################################################################
 

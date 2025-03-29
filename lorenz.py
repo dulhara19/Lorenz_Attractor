48c11a17b605b5767e1e5cbf42458e3f57528f65
dulhara <wkldulhara@gmail.com>
2025-03-29 23:49:52 +0530

    up

--- lorenz.py
+++ lorenz.py
@@ -46,58 +46,58 @@
 
 # ###############################################################################
 
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
 
-# def lorenz(xyz, *, s=10, r=28, b=2.667):
-#     x, y, z = xyz
-#     x_dot = s * (y - x)
-#     y_dot = r * x - y - x * z
-#     z_dot = x * y - b * z
-#     return np.array([x_dot, y_dot, z_dot])
+def lorenz(xyz, *, s=10, r=28, b=2.667):
+    x, y, z = xyz
+    x_dot = s * (y - x)
+    y_dot = r * x - y - x * z
+    z_dot = x * y - b * z
+    return np.array([x_dot, y_dot, z_dot])
 
 
-# # Time step and number of iterations
-# dt = 0.01
-# num_steps = 5000  # Reduced for smoother animation
+# Time step and number of iterations
+dt = 0.01
+num_steps = 5000  # Reduced for smoother animation
 
-# # Initialize array for points
-# xyzs = np.empty((num_steps + 1, 3))
-# xyzs[0] = (0., 1., 1.05)
+# Initialize array for points
+xyzs = np.empty((num_steps + 1, 3))
+xyzs[0] = (0., 1., 1.05)
 
-# # Compute Lorenz attractor points
-# for i in range(num_steps):
-#     xyzs[i + 1] = xyzs[i] + lorenz(xyzs[i]) * dt
+# Compute Lorenz attractor points
+for i in range(num_steps):
+    xyzs[i + 1] = xyzs[i] + lorenz(xyzs[i]) * dt
 
-# # Create figure and 3D axis
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-# ax.set_xlim([-20, 20])
-# ax.set_ylim([-30, 30])
-# ax.set_zlim([0, 50])
+# Create figure and 3D axis
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.set_xlim([-20, 20])
+ax.set_ylim([-30, 30])
+ax.set_zlim([0, 50])
 
-# ax.set_xlabel("X Axis")
-# ax.set_ylabel("Y Axis")
-# ax.set_zlabel("Z Axis")
-# ax.set_title("Lorenz Attractor Animation")
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+ax.set_zlabel("Z Axis")
+ax.set_title("Lorenz Attractor Animation")
 
-# # Initialize line object
-# line, = ax.plot([], [], [], lw=1)
-
-
-# # Update function for animation
-# def update(num):
-#     line.set_data(xyzs[:num, 0], xyzs[:num, 1])
-#     line.set_3d_properties(xyzs[:num, 2])
-#     return line,
+# Initialize line object
+line, = ax.plot([], [], [], lw=1)
 
 
-# # Animate using FuncAnimation
-# ani = animation.FuncAnimation(fig, update, frames=num_steps, interval=1, blit=True)
+# Update function for animation
+def update(num):
+    line.set_data(xyzs[:num, 0], xyzs[:num, 1])
+    line.set_3d_properties(xyzs[:num, 2])
+    return line,
 
-# plt.show()
+
+# Animate using FuncAnimation
+ani = animation.FuncAnimation(fig, update, frames=num_steps, interval=1, blit=True)
+
+plt.show()
 
 #####################################################################################
 
@@ -202,3 +202,60 @@ plt.show()
 # plt.show()
 
 
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+
+# Lorenz system function
+def lorenz(xyz, s=10, r=28, b=2.667):
+    x, y, z = xyz
+    x_dot = s * (y - x)
+    y_dot = r * x - y - x * z
+    z_dot = x * y - b * z
+    return np.array([x_dot, y_dot, z_dot])
+
+# Simulation parameters
+dt = 0.01
+num_steps = 5000  # Animation frames
+
+# Number of trajectories (1000 initial conditions)
+num_curves = 1000  
+initial_conditions = np.random.uniform(low=-2, high=2, size=(num_curves, 3))  # Generate 1000 random initial values in the range [-2, 2]
+
+# Generate trajectories
+trajectories = []
+for init in initial_conditions:
+    xyzs = np.empty((num_steps + 1, 3))
+    xyzs[0] = init
+    for i in range(num_steps):
+        xyzs[i + 1] = xyzs[i] + lorenz(xyzs[i]) * dt
+    trajectories.append(xyzs)
+
+# Set up the figure
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.set_xlim([-20, 20])
+ax.set_ylim([-30, 30])
+ax.set_zlim([0, 50])
+
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+ax.set_zlabel("Z Axis")
+ax.set_title("Animated Lorenz Attractors")
+
+# Create lines for the curves
+colors = plt.cm.viridis(np.linspace(0, 1, num_curves))  # Use a colormap for a smooth color gradient
+lines = [ax.plot([], [], [], lw=0.5, color=colors[i])[0] for i in range(num_curves)]
+
+# Update function for animation
+def update(num):
+    for i, line in enumerate(lines):
+        xyzs = trajectories[i]
+        line.set_data(xyzs[:num, 0], xyzs[:num, 1])
+        line.set_3d_properties(xyzs[:num, 2])
+    return lines
+
+# Animate
+ani = animation.FuncAnimation(fig, update, frames=num_steps, interval=1, blit=True)
+
+plt.show()

f953a9d249e7050e5b77d88d0d58f13518fd9b33
dulhara <wkldulhara@gmail.com>
2025-03-30 23:58:11 +0530

    new file added

--- lorenz.py
+++ lorenz.py
@@ -20,7 +20,7 @@ def lorenz(xyz, *, s=10, r=28, b=2.667):
     x_dot = s*(y - x)
     y_dot = r*x - y - x*z
     z_dot = x*y - b*z
-    return np.array([x_dot, y_dot, z_dot])
+    return np.array([x_dot, y_dot, z_dot])  #
 
 
 dt = 0.01

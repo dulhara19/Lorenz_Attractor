7d6441f6d034c5f7cb400c38982ee72bf96a15c7
dulhara <wkldulhara@gmail.com>
2025-04-03 23:59:57 +0530

    new

--- chaotic_stock_price_model.py
+++ chaotic_stock_price_model.py
@@ -1,5 +1,6 @@
 #---------A simple chaotic stock price model based on a modified Lorenz system--------
 
+# # Import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
